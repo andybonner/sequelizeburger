5d69871f2b187e73e5a771383d0b6a3540242105
Andy Bonner <abonner@duke.edu>
2017-08-31 15:51:58 -0400

    add burger_description

--- models/burger.js
+++ models/burger.js
@@ -10,6 +10,7 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
+    burger_description: DataTypes.STRING,
     devoured_by: {
       type: DataTypes.STRING,
       defaultValue: null
@@ -28,28 +29,3 @@ module.exports = function(sequelize, DataTypes) {
 };
 
 // is imported, as part of parent folder "models", by api-routes.js
-
-
-
-// var ORM = require("../config/orm");
-
-// var burger = {
-//   selectAll: function(callback) {
-//     ORM.selectAll("burgers", function(res){
-//       callback(res);
-//     });
-//   },
-//   insertOne: function(cols, vals, callback){
-//     ORM.insertOne("burgers", cols, vals, function(res){
-//       callback(res);
-//     })
-//   },
-//   updateOne: function(objColVals, condition, callback){
-//     ORM.updateOne("burgers", objColVals, condition, function(res){
-//       callback(res);
-//     })
-//   }
-// }
-
-// module.exports = burger;
-// // is imported by burgers_controller.js
