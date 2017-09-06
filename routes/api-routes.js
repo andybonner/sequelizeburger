879ec33b37df33f4401a295adc0dae30e03902b1
Andy Bonner <abonner@duke.edu>
2017-09-06 11:31:44 -0400

    POST route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,13 @@
-var db = require("../models");
+const db = require("../models");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Burger.findAll({
       include: [db.Inventor]
-    }).then(function(dbResponse) {
-      // console.log("dbResponse", dbResponse);
-      var hbarsObj = {
-        burgers: dbResponse
+    }).then(function(dbBurgers) {
+      // console.log("dbBurgers", dbBurgers);
+      let hbarsObj = {
+        burgers: dbBurgers
       }
       // console.log("hbarsObj", hbarsObj);
       res.render("index", hbarsObj);
@@ -15,12 +15,11 @@ module.exports = function(app) {
   });
 
   app.post("/", function(req, res) {
-    db.Burger.create(req.body).then(function(dbResponse) {
-      var hbarsObj = {
-        burgers: dbResponse
-      }
-      res.render("index", hbarsObj);
-    })
+    console.log("req.body", req.body);
+    db.Inventor.create(req.body).then((dbInventor) => {
+      req.body.InventorId = dbInventor.dataValues.id;
+      db.Burger.create(req.body).then( () => res.redirect('/'));
+    });
   });
 
   app.put("/:id", function(req, res) {
