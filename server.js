09898fa07003df2486f628b2d86b165955db6736
Andy Bonner <abonner@duke.edu>
2017-09-07 18:30:13 -0400

    remove "force: true"

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const routes = require("./routes/api-routes")(app);
 
 // app.use("/", routes);
 
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log("Listening on port", PORT);
   });
