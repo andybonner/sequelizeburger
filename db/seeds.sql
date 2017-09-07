aeebfc7399cda7a95a75eae14670d45b55055bb3
Andy Bonner <abonner@duke.edu>
2017-09-07 08:10:48 -0400

    remove old folders

--- db/seeds.sql
+++
@@ -1,8 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES
-("quarter pounder", false),
-("bacon cheeseburger", false),
-("mushroom swiss burger", false),
-("sriracha avacodo turkey burger", false)
